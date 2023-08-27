d877f4020da56065a6889d770edb7bbbf2f43718
Johannes Coetsee <johannes.coetsee@ktu.edu>
2023-08-27 12:02:24 +0200

    Final functions and main logic of requested method

--- DateCustom.java
+++ DateCustom.java
@@ -11,15 +11,6 @@ public class DateCustom {
     public DateCustom(){
 
     }
-    public void setYear(int _year){
-        year = _year;
-    }
-    public void setMonth(int _month){
-        month = _month;
-    }
-    public void setDay(int _day){
-        day = _day;
-    }
     public int getYear(){
         return year;
     }
@@ -29,9 +20,61 @@ public class DateCustom {
     public int getDay(){
         return day;
     }
+    public void increaseDate(){
+        int max = 0;
+        if(month == 2){
+            if(isLeapYear(year)){
+                max = 29;
+            }else{
+                max =28;
+            }
+        }else{
+            max = months[month-1];
+        }
+        day++;
+        if(day > max){
+            day = 1;
+           month++;
+           if(month>12){
+               month = 1;
+               year++;
+           }
+        }
+    }
+    private boolean isLeapYear(int _year){
+        String sYear = _year + "";
+        String lastDigits = "";
+        if(sYear.length()==3){
+            lastDigits = sYear.substring(1,3);
+            if(lastDigits.equals("00")){
+                return _year%400 == 0;
+            }else{
+                return _year%4 == 0;
+            }
+        }else if(sYear.length() == 4){
+            lastDigits = sYear.substring(2,4);
+            if(lastDigits.equals("00")){
+                return _year%400 == 0;
+            }else {
+                return _year % 4 == 0;
+            }
+        }else{
+            return _year%4 == 0;
+        }
+    }
     public String toString(){
-        String dateString = year + "-"+month+"-"+day;
-        System.out.println(dateString);
+        String dateString = formatWithZeroes(year,4) + "-"+formatWithZeroes(month,2)+"-"+formatWithZeroes(day,2);
         return dateString;
+    }
+    public String toPalindromeString(){
+        String dateString = formatWithZeroes(year,4) + formatWithZeroes(month,2)+formatWithZeroes(day,2);
+        return dateString;
+    }
+    private String formatWithZeroes(int date, int digits){
+        String sDate = date + "";
+        while(sDate.length()< digits){
+            sDate = "0" +sDate;
+        }
+        return sDate;
     }
 }
--- FindDates.java
+++ FindDates.java
@@ -1,8 +1,16 @@
 
 public class FindDates {
     public static void main(String[] Args){
-        DateCustom date_1 = new DateCustom(1);
-        System.out.println(checkDateForPalindrome(date_1.toString()));
+        printBonusDatesBetween(0,10000);
+    }
+    public static void printBonusDatesBetween(int fromYear, int toYear){
+        DateCustom date_1 = new DateCustom(fromYear);
+        while(date_1.getYear() < toYear){
+            if(checkDateForPalindrome(date_1.toPalindromeString())){
+                System.out.println(date_1);
+            }
+            date_1.increaseDate();
+        }
     }
     public static Boolean checkDateForPalindrome(String date){
         String reverseDate = reverseString(date);
